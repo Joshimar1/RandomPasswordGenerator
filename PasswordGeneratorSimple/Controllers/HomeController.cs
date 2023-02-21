727ce072343bba8f7d449f002136abd7763f05f0
Joshimar1 <joshimarmora@gmail.com>
2023-02-21 08:01:07 -0500

    Simple Password Generator
    This generates a random password made up of upper case letters, lower case letters, numbers, and symbols. The length of the password is determined by the number entered by the user.

---
+++ PasswordGeneratorSimple/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PasswordGeneratorSimple.Models;
+using System.Diagnostics;
+
+namespace PasswordGeneratorSimple.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GeneratePassword(int length)
+        {
+            var password = PasswordGenerator.Generate(length);
+            return Content(password);
+        }
+
+
+
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PasswordGeneratorSimple/PasswordGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace PasswordGeneratorSimple
+{
+    public class PasswordGenerator
+    {
+        private const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[{]};:<>,.?/";
+
+        public static string Generate(int length)
+        {
+            var random = new Random();
+            var chars = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                chars[i] = validChars[random.Next(validChars.Length)];
+            }
+
+            return new string(chars);
+        }
+    }
+}
